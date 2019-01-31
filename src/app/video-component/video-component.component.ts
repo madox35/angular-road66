cb17b0d03d04a4aa28d41689bb09cf1e3903fa7f
Hugo Jové <hugo.jove2@gmail.com>
2019-01-31 10:08:18 +0100

    feat: video component

---
+++ src/app/video-component/video-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-component',
+  templateUrl: './video-component.component.html',
+  styleUrls: ['./video-component.component.css']
+})
+export class VideoComponentComponent {
+  url_mp4 = "test.mp4"
+  url_ogg = "test.ogg"
+}
